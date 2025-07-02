21d1b94c15ece3fee0b89a3fce3b71bdd8751694
Max de Ruiter <m.j.deruiter99@hotmail.com>
2025-07-02 18:15:17 +0200

    changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,19 @@
-import '../styling/header.css'
-import profile from '../media/max-laptop.jpg';
+import "../styling/header.css";
+import profile from "../media/max-laptop.jpg";
 
 function Header() {
-    return (
-        <header id='header'>
-            <div className='header-intro'>
-                <h2 className='header-hidden'>I am Max de Ruiter</h2> 
-                <h1>USER-CENTERED DESIGNER</h1>
-                <h3>Available for work</h3>
-            </div>
-            <div className='header-profile'>
-                <img src={profile} className='header-profile-image' alt='profile' />
-            </div>    
-        </header>
-    );
-  }
-  
-  export default Header;
-  
+  return (
+    <header id="header">
+      <div className="header-intro">
+        <h2 className="header-hidden">I am Max de Ruiter</h2>
+        <h1>Full-stack Developer & UX/UI Designer</h1>
+        <h3>Full-stack developer @ government agency</h3>
+      </div>
+      <div className="header-profile">
+        <img src={profile} className="header-profile-image" alt="profile" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
